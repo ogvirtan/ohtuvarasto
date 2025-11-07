5c7114d94a894a435cc8d1e836c3825f97376dd6
Olli Virtanen <ovirtanen96@gmail.com>
2025-11-07 16:07:19 +0200

    Korjataan pylint-virheet

--- src/varasto.py
+++ src/varasto.py
@@ -1,5 +1,5 @@
 class Varasto:
-    def __init__(self, tilavuus, alku_saldo = 0):
+    def __init__(self, tilavuus, alku_saldo = 0):# pylint: disable=too-many-statements
         if tilavuus > 0.0:
             self.tilavuus = tilavuus
         else:
@@ -16,7 +16,8 @@ class Varasto:
             # täyteen ja ylimäärä hukkaan!
             self.saldo = tilavuus
 
-    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
+    # huom: ominaisuus voidaan myös laskea.
+    # Ei tarvita erillistä kenttää viela_tilaa tms.
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
@@ -28,21 +29,18 @@ class Varasto:
         else:
             self.saldo = self.tilavuus
 
-    def ota_varastosta(self, maara, useless=True, useless2=True, useless3=True):
-            if maara < 0:
-                if useless==useless2:
-                    if useless==useless3:
-                        pass
-                return 0.0
-            if maara > self.saldo:
-                kaikki_mita_voidaan = self.saldo
-                self.saldo = 0.0
+    def ota_varastosta(self, maara):
+        if maara < 0:
+            return 0.0
+        if maara > self.saldo:
+            kaikki_mita_voidaan = self.saldo
+            self.saldo = 0.0
 
-                return kaikki_mita_voidaan
+            return kaikki_mita_voidaan
 
-            self.saldo = self.saldo - maara
+        self.saldo = self.saldo - maara
 
-            return maara
+        return maara
 
     def __str__(self):
         return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
